ad948fd2b7bd0c316ae700bd682d5c70b250758f
Marcus Grant <marcusfg@gmail.com>
2018-05-09 10:34:16 -0400

    move test script back to root

--- test_rsync_snap.py
+++ test_rsync_snap.py
@@ -17,13 +17,13 @@ from rsync_snap import (
 )
 
 
-class ArgParseSourceTests(unittest.TestCase):
+class TestArgParsing(unittest.TestCase):
     """Tests involving the use of argument parsing"""
     # Below is the expected output#
     """usage: rsync_snap.py [-h] BACKUP_PROFILE
 rsync_snap.py: error: the following arguments are required: BACKUP_PROFILE"""
 
-    def test_no_arg(self):
+    def no_arg(self):
         """Test for printing the right response for running without args"""
         no_arg_output = subprocess.run('./rsync_snap.py',
                                        stdout=subprocess.PIPE,
@@ -34,7 +34,7 @@ rsync_snap.py: error: the following arguments are required: BACKUP_PROFILE"""
         self.assertIn('error', no_arg_output)
         self.assertIn(BACKUP_PROFILE_META, no_arg_output)
 
-    def test_h_for_help(self):
+    def h_for_help(self):
         """Test that entering '-h' brings up help output"""
         h_arg_output = subprocess.run('./rsync_snap.py -h'.split(),
                                       stdout=subprocess.PIPE,
@@ -45,7 +45,7 @@ rsync_snap.py: error: the following arguments are required: BACKUP_PROFILE"""
         for substring in expected_outputs:
             self.assertIn(substring, h_arg_output)
 
-    def test_that_h_arg_equals_help_arg(self):
+    def that_h_arg_equals_help_arg(self):
         """Test that '-h' as an arg does the same thing as '--help'"""
         h_arg_output = subprocess.run('./rsync_snap.py -h'.split(),
                                       stdout=subprocess.PIPE,
@@ -57,7 +57,7 @@ rsync_snap.py: error: the following arguments are required: BACKUP_PROFILE"""
         help_arg_output = help_arg_output.stdout.decode('UTF-8')
         self.assertEqual(h_arg_output, help_arg_output)
 
-    def test_is_valid_profile_name_valid(self):  # pylint: disable=invalid-name
+    def is_valid_profile_name_valid(self):  # pylint: disable=invalid-name
         """Test that is_valid_profile_name returns a profile name
         when valid characters are given to it"""
         expected_profile = 'abc-123_XYZ'
@@ -66,7 +66,7 @@ rsync_snap.py: error: the following arguments are required: BACKUP_PROFILE"""
         self.assertEqual(expected_profile,
                          is_valid_profile_name(args.backup_profile))
 
-    def test_is_valid_profile_name_invalid(self):
+    def is_valid_profile_name_invalid(self):
         """Test that is_valid_profile_name exits with an error message
         and the right error message when invalid characters are used"""
         given_profiles = [
@@ -74,11 +74,11 @@ rsync_snap.py: error: the following arguments are required: BACKUP_PROFILE"""
             'a+', 'a=', 'a:', 'a,', 'a.', 'a?',
         ]
         for profile in given_profiles:
-            with self.assertRaises(SystemExit) as cm:
+            with self.assertRaises(SystemExit) as exit_case:
                 is_valid_profile_name(profile)
-            self.assertEqual(cm.exception.code, PROFILE_NAME_ERR)
+            self.assertEqual(exit_case.exception.code, PROFILE_NAME_ERR)
 
-    def test_invalid_profile_name_fail(self):
+    def invalid_profile_name_fail(self):
         """Test that when script is given invalid profile,
         it fails with the right message"""
         given_profiles = [
@@ -95,9 +95,18 @@ rsync_snap.py: error: the following arguments are required: BACKUP_PROFILE"""
             self.assertEqual(invalid_output, '')
             self.assertIn(PROFILE_NAME_ERR, invalid_err)
 
-    def test_invalid_config(self):
-        """Test if dictionary from parsed from config file is empty"""
-        configs = {
-            'test1': {}
+
+class TestConfigParsing(unittest.TestCase):
+    """Testing related to parsing the config file for rsync_snap"""
+
+    def parse_config_bad_profile_key(self):
+        """Test if any key besides 'profile' in a config returns an empty
+        dictionary"""
+        profile_dict = {
+            'correctProfile': {
+                'src': '/tmp',
+                'dst': '/var/lib'
+            }
         }
-        self.assertEqual(parse_config('./.test-config-fail.txt'), configs)
+        config_file = './tests/invalid-profiles.conf'
+        self.assertEqual(parse_config(config_file), profile_dict)

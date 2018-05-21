e4079e8c35c809006de62d6c3c4f0c6b739ced1e
Marcus Grant <marcusfg@gmail.com>
2018-05-21 19:30:32 -0400

    continuation

---
+++ test-rsync-snap.go
@@ -0,0 +1,19 @@
+package main
+
+func TestUsageOnEmptyCommand (t *testing.T) {
+	if 
+}
+// func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
+//     buffer := new(bytes.Buffer)
+//     GenerateQRCode(buffer, "555-2368", Version(1))
+//
+//     if buffer.Len() == 0 {
+//         t.Errorf("No QRCode generated")
+//     }
+//
+//     _, err := png.Decode(buffer)
+//
+//     if err != nil {
+//         t.Errorf("Generated QRCode isn't a PNG: %s", err)
+//     }
+// }
